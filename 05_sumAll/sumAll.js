a34798b3114ad08e3dba96c0283fc8d6498e2904
vicdd <victorrobcam@gmail.com>
2023-06-29 17:59:20 -0300

    solution commit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if (typeof end != "number" || start < 0) {
+        return 'ERROR';
+    }
+
+    // If start is bigger than end, then swap both
+    start > end ? [start, end] = [end, start] : null;
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
