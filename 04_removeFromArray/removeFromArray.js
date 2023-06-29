a63d8cae96fc8e37e4ec25c082d4138f46eedc29
vicdd <victorrobcam@gmail.com>
2023-06-29 17:40:03 -0300

    solution commit

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,30 @@
-const removeFromArray = function() {
+function getAllIndexes(array, element) {
+    let indexes  = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            indexes.push(i);
+        }
+    }
 
+    return indexes;
+}
+
+function removeElement(array, indexes) {
+    for (let i = 0; i < indexes.length; i++) {
+        array.splice(indexes[i], 1);
+    }
+
+    return array;
+}
+
+const removeFromArray = function(...args) {
+    let array = args[0];
+    for (let i = 1; i < args.length; i++) {
+        indexes = getAllIndexes(array, args[i])
+        array = removeElement(array, indexes);
+    }
+
+    return array;
 };
 
 // Do not edit below this line
