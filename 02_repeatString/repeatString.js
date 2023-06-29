99782dfe92db3e18e71468cf112893c9e6353f9d
vicdd <victorrobcam@gmail.com>
2023-06-29 13:53:00 -0300

    solution commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    let stringRepeated = '';
 
+    if (times < 0) return 'ERROR';
+
+    for (let i = 0; i < times; i++) {
+        stringRepeated = stringRepeated.concat(string);
+    }
+    
+    return stringRepeated;
 };
 
 // Do not edit below this line
