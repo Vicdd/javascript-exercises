32cf0dc4e1589cec3699a458e88190e96f70bcda
vicdd <victorrobcam@gmail.com>
2023-07-04 11:36:46 -0300

    solution commit

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = 0;
+    let person;
 
+    people.forEach(e => {
+        let yearOfDeath = e.yearOfDeath ?? 2023;
+        if (yearOfDeath - e.yearOfBirth > oldest) {
+            oldest = yearOfDeath - e.yearOfBirth;
+            person = e;
+        }
+    });
+
+    return person;
 };
 
 // Do not edit below this line
