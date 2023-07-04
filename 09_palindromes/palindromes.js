cbd433ec8336b08be76fb58813ed88dae82dc9d5
vicdd <victorrobcam@gmail.com>
2023-07-04 10:49:13 -0300

    solution commit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/[^\w]+/g, '').toLowerCase();
 
+    for (let i = 0, n = str.length; i < n; i++) {
+        if (str[i] != str[n - i - 1]) return false;
+    }
+    
+    return true;
 };
 
 // Do not edit below this line
