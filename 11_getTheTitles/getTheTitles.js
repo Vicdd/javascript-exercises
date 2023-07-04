97c309cc5e3643cb166d33e94b4a1a2aeaa50410
vicdd <victorrobcam@gmail.com>
2023-07-04 11:01:50 -0300

    solution commit

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    let array = [];
+    books.forEach(e => {
+        array.push(e.title);
+    });
+    return array;
 };
 
 // Do not edit below this line
