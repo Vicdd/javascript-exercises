5826ccb2094e4e7134770d64b6704d9311132fdc
vicdd <victorrobcam@gmail.com>
2023-07-04 10:34:59 -0300

    solution submit

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  array.forEach(e => sum += e);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let res = 1;
+  args.forEach(e => res *= e);
+  return res;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let res = 1;
+  for (let i = 1; i <= y ; i++) res *= x;
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let res = 1;
+  for (let i = 1; i <= x ; i++) res *= i;
+  return res;
 };
 
 // Do not edit below this line
